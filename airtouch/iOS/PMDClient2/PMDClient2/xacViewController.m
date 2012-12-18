5aff7824dbae3906ab17a0dd996825cceb3fcf9f
hotnAny <xiangchen@cmu.edu>
2012-12-18 14:49:40 +0800

    updated PMD client

--- airtouch/iOS/PMDClient2/PMDClient2/xacViewController.m
+++ airtouch/iOS/PMDClient2/PMDClient2/xacViewController.m
@@ -23,9 +23,16 @@ bool isConnected = false;
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     _udpSocket = [[AsyncUdpSocket alloc] initWithDelegate:self];
+    [_udpSocket setDelegate:self];
+    
+    //
     // change the ip address
-    ipAddr = @"128.237.226.252";
+    //
+//    ipAddr = @"128.237.238.104";
+//    ipAddr = @"128.237.226.252";
+    ipAddr = @"192.168.1.19";
     port = 21027;
+    _tag = 0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,23 +47,28 @@ bool isConnected = false;
     if(!isConnected)
     {
         NSError *error = nil;
+        
+        if(![_udpSocket enableBroadcast:YES error:&error])
+        {
+            NSLog(@"Error enabling broadcast: %@", error);
+            return;
+        }
+        
         if (![_udpSocket bindToPort:port error:&error])
         {
             NSLog(@"Error binding: %@", error);
             return;
         }
         
-        [_udpSocket receiveWithTimeout:-1 tag:0];
-        
         isConnected = true;
     }
     
     if(isConnected)
     {
-    //    NSData *data = [[NSData alloc] init];
         NSString *msg = @"connect me!";
         NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
-        [_udpSocket sendData:data toHost:ipAddr port:port withTimeout:-1 tag:_tag];
+        // [_udpSocket sendData:data toHost:ipAddr port:port withTimeout:-1 tag:_tag];
+        [_udpSocket receiveWithTimeout:-1 tag:_tag];
     }
 }
 
@@ -75,8 +87,7 @@ bool isConnected = false;
 	{
         NSLog(@"Error converting received data into UTF-8 String");
 	}
-    
-    
+    [_udpSocket receiveWithTimeout:-1 tag:_tag];
 	return YES;
 }
 @end
