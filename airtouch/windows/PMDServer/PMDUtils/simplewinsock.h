1182504fab70d33d017696e93e5173184dc2fd34
unknown <julenka@cs.cmu.edu>
2012-12-18 21:15:25 -0500

    Adding a simple client and server application.
    Client can send input from console
    Server echos input

---
+++ airtouch/windows/PMDServer/PMDUtils/simplewinsock.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <WinSock2.h>
+#include <iostream>
+#include <process.h>
+#include <Windows.h>
+#include <sstream>
+
+using namespace std;
+
+#define DEFAULT_BUFLEN 512 // Not currently used
+
+// information on how to use sockets for windows can be found at
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms738566(v=vs.85).aspx
+
+// Initialize WinSock2.2 DLL 
+// low word = major, highword = minor
+HRESULT initWinsock(WSADATA* wsaData, WORD* wVer)
+{
+	int nRet = WSAStartup( *wVer, wsaData );
+	if( nRet == SOCKET_ERROR ) { 
+		cout << "Failed to init Winsock library" << endl;
+		return -1;
+	} 
+	return 0;
+}
+
+HRESULT bindSocket(SOCKET* hSocket, sockaddr_in* socketInfo)
+{
+	HRESULT hr = bind( *hSocket, (sockaddr*)socketInfo, sizeof(sockaddr) );
+	if( hr == SOCKET_ERROR ) { 
+		cout << "Failed to bind socket" << endl;
+		//shutdown( hSock ); 
+		closesocket( *hSocket );
+		return -1;
+	} 
+	return 0;
+}
+
+HRESULT connectSocket(SOCKET* hSocket, sockaddr_in* socketInfo)
+{
+	HRESULT hr = connect( *hSocket, (sockaddr*)socketInfo, sizeof( sockaddr ) );  
+	if( hr == SOCKET_ERROR ) { 
+		cout << "Connection to server failed" << endl;
+		closesocket( *hSocket );
+		return -1;
+	} 
+	return 0;
+}
+
+HRESULT getClientConnection(SOCKET* hSock, SOCKET* hClient)
+{
+		HRESULT hr = listen( *hSock, 5 ); // connection backlog queue set to 10 
+		if( hr == SOCKET_ERROR ) 
+		{ 
+			int nErr = WSAGetLastError();
+			if( nErr == WSAECONNREFUSED ) { 
+				cout << "Failed to listen, connection refused" << endl;
+			} 
+			else { 
+				cout << "Call to listen failed: " << nErr << endl;
+			} 
+			closesocket( *hSock );
+			return -1;
+		} 
+		// connect 
+		sockaddr_in saClient = {0};
+		int nSALen = sizeof( sockaddr );
+		*hClient = accept( *hSock, (sockaddr*)&saClient, &nSALen );
+		if( *hClient == INVALID_SOCKET ) { 
+			cout << "Invalid client socket, connection failed" << endl;
+			closesocket( *hClient );
+			return -1;
+		} 
+		cout << "Connection estabilished" << endl;
+		return 0;
+}
+
+// Send data on a socket
+// data: data to send
+// len: length of data
+// flags: any flags to use.
+void sendData(SOCKET socket, const char* data, int len, int flags)
+{
+	int iPos  = 0;
+	int nLeft = len;
+	int nData = 0;
+	do
+	{ 
+		nData = send( socket, &data[iPos], nLeft, 0 );
+		if( nData == SOCKET_ERROR ) { 
+			cout << "Error sending data" << endl;
+			break;
+		} 
+		nLeft -= nData;
+		iPos  += nData;
+	} while( nLeft > 0 );
+}
+
+// Receive data on a socket. Returns number of bits filled, or -1 if error
+// buffer: buffer to fill
+// len: maximum length of the buffer
+// mustFill: whether the buffer must be filled
+
+int receiveData(SOCKET socket, char* buffer, int len, bool mustFill)
+{
+	int nLeft  = len;
+	int iPos   = 0;
+	int nData  = 0;
+
+	// clear data buffer 
+	memset( buffer, 0, len );
+	do
+	{ 
+		nData = recv( socket, &buffer[iPos], nLeft, 0 );
+		if( nData == SOCKET_ERROR ) { 
+			cout << "Error receiving data" << endl;
+			return -1;
+		} 
+		if(!mustFill)
+			return nData;
+		
+		nLeft -= nData;
+		iPos  += nData;
+		cout << "received " << nData << " bits, " << nLeft << " left";
+	} while( nLeft > 0 );
+	return len - nLeft;
+}
---
+++ airtouch/windows/PMDServer/PMDUtils/strutils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// @julenka:
+// right now I am a bit too lazy to include the boost library
+// but i should replace these by just using the boost library
+// http://www.boost.org/doc/libs/1_52_0/more/getting_started/windows.html
+// 
+
+
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+// from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+        return ltrim(rtrim(s));
+}
