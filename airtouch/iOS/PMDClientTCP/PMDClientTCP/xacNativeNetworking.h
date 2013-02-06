32092dd590d687b5f1c8284782270b36ed61d617
hotnAny <xiangchen@cmu.edu>
2013-02-06 15:48:37 -0500

    client now visualizes finger movement

---
+++ airtouch/iOS/PMDClientTCP/PMDClientTCP/xacNativeNetworking.h
@@ -0,0 +1,13 @@
+//
+//  xacNativeNetworking.h
+//  PMDClientTCP
+//
+//  Created by Xiang 'Anthony' Chen on 2/5/13.
+//  Copyright (c) 2013 hotnAny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xacNativeNetworking : NSObject
+
+@end
---
+++ airtouch/iOS/PMDClientTCP/PMDClientTCP/xacNativeNetworking.m
@@ -0,0 +1,13 @@
+//
+//  xacNativeNetworking.m
+//  PMDClientTCP
+//
+//  Created by Xiang 'Anthony' Chen on 2/5/13.
+//  Copyright (c) 2013 hotnAny. All rights reserved.
+//
+
+#import "xacNativeNetworking.h"
+
+@implementation xacNativeNetworking
+
+@end
