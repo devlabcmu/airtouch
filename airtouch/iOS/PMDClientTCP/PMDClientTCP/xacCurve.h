a0fd878c02011caa13901513b6426f7938be56ec
hotnAny <xiangchen@cmu.edu>
2013-02-08 14:38:32 -0500

    added visualizing finger movement feature

---
+++ airtouch/iOS/PMDClientTCP/PMDClientTCP/xacCurve.h
@@ -0,0 +1,13 @@
+//
+//  xacCurve.h
+//  PMDClientTCP
+//
+//  Created by Xiang 'Anthony' Chen on 2/6/13.
+//  Copyright (c) 2013 hotnAny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xacCurve : NSObject
+
+@end
---
+++ airtouch/iOS/PMDClientTCP/PMDClientTCP/xacCurve.m
@@ -0,0 +1,13 @@
+//
+//  xacCurve.m
+//  PMDClientTCP
+//
+//  Created by Xiang 'Anthony' Chen on 2/6/13.
+//  Copyright (c) 2013 hotnAny. All rights reserved.
+//
+
+#import "xacCurve.h"
+
+@implementation xacCurve
+
+@end
