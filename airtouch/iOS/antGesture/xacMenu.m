6a56d4f0ce119fecc1721cd88f6935442e238a14
hotnAny <xiangchen@cmu.edu>
2013-02-19 20:23:20 -0500

    finished a rough demo of in-air gesture

---
+++ airtouch/iOS/antGesture/xacMenu.m
@@ -0,0 +1,31 @@
+//
+//  xacMenu.m
+//  antGesture
+//
+//  Created by Xiang 'Anthony' Chen on 2/19/13.
+//  Copyright (c) 2013 hotnAny. All rights reserved.
+//
+
+#import "xacMenu.h"
+
+@implementation xacMenu
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
