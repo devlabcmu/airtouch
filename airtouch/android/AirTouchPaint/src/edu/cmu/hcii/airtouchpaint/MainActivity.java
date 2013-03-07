f2a1da6f5c86f523f7847fa69d89e6c791ecc2ce
julenka <julenka@cs.cmu.edu>
2013-03-07 18:08:02 -0500

    AirTouchPaint skeleton

---
+++ airtouch/android/AirTouchPaint/src/edu/cmu/hcii/airtouchpaint/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.cmu.hcii.airtouchpaint;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+}
